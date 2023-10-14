8cebed746208d3fd7b1e369e17ae70703315b4f2
Dhanavidhya M <127582714+VIDHYA1226@users.noreply.github.com>
2023-10-14 17:41:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ApiRecords  from './components/api_records';
+
+
+function App() {
+  return (
+    <div>
+      <ApiRecords />
+    </div>
+  );
+}
+
+export default App;
